4bc8c6bd6683a5d6acf1bd0f765734894d60a7c4
baus <baus@d4b189cd-b497-4395-a474-6445c121c4db>
2007-04-01 00:49:12 +0000

    use correct directory variable
    git-svn-id: http://svn.baus.net/switchflow/max@48 d4b189cd-b497-4395-a474-6445c121c4db

--- sfrp/app/gensvnversion.sh
+++ sfrp/app/gensvnversion.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dir=$(dirname `echo $0 | sed -e "s,^\([^/]\),$(pwd)/\1,"`)
 echo -n 'const char* SVNVersion(void) { const char* SVN_Version = "' > $dir/SVNVersion.cpp
-svnversion -n $dirname/.. >> $dir/SVNVersion.cpp;
+svnversion -n $dir/.. >> $dir/SVNVersion.cpp;
 echo '"; return SVN_Version; }'  >> $dir/SVNVersion.cpp
