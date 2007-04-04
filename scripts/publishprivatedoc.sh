92abcd2affa203b2b131fda33635d8f2cc62dd2c
baus <baus@d4b189cd-b497-4395-a474-6445c121c4db>
2007-04-04 21:31:19 +0000

    add publishprivate script.
    git-svn-id: http://svn.baus.net/switchflow/trunk@56 d4b189cd-b497-4395-a474-6445c121c4db

---
+++ scripts/publishprivatedoc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+dir=$(dirname `echo $0 | sed -e "s,^\([^/]\),$(pwd)/\1,"`)
+
+pushd $dir/../switchflow/
+doxygen switchflow.dox
+popd
+rsync -r $dir/../switchflow/docs/html/ baus@baus.net:/var/www/bausnet/static/switchflow/private/docs
+
