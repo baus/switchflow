4da6d85979b5c5be5ccb26436d1b419697d16bd8
Chris Baus <christopher@baus.net>
2013-08-24 15:27:17 -0700

    Update rebuildall.sh
    Remove reference to thirdparty libraries. Now using distro supplied versions.

--- rebuildall.sh
+++ rebuildall.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 dir=$(dirname `echo $0 | sed -e "s,^\([^/]\),$(pwd)/\1,"`)
 
-rm -f $dir/thirdparty/lib/debug/*
-rm -f $dir/thirdparty/lib/release/*
-
-sh $dir/switchflow/buildthirdparty.sh
-sh $dir/configbuild.sh
-
 cd $dir/release/sfrp
 make clean
 make
