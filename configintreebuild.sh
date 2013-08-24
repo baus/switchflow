33f39168544a5c590093210b7c7e959b941c1c8c
Christopher Baus <christopher@baus.net>
2013-08-24 15:58:31 -0700

    added comments.

--- configintreebuild.sh
+++ configintreebuild.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+##
+## This creates the make files and dumps the object files in the source tree
+## This is useful for debugging with GDB.
+
 dir=$(dirname `echo $0 | sed -e "s,^\([^/]\),$(pwd)/\1,"`)
 
 cd $dir/sfrp
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug
+cd $dir
 
