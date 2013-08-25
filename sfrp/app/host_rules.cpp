40de152b37890af7074ddd84cfc2b70434b88198
Christopher Baus <christopher@baus.net>
2013-08-24 18:43:58 -0700

    added host rules back in

--- sfrp/app/host_rules.cpp
+++ sfrp/app/host_rules.cpp
@@ -16,17 +16,17 @@ void build_host_rules(const switchflow::util::config_file& config,
     httplib::url url(config["virtual-host"][i]["default-forward-url"].read<std::string>().c_str(), true);
     host_rules rules(url,
                      config["virtual-host"][i]["preserve-host"].read<bool>());
-    /**    
     for(size_t cur_rule = 0;
         cur_rule < config["virtual-host"][i]["rule"].get_array_size();
         ++cur_rule){
-      switchflow::util::config_file::value cur_rule_value =   
-      config["virtual-host"][i]["rule"][cur_rule];
+      
+      switchflow::util::config_file::value cur_rule_value 
+	= config["virtual-host"][i]["rule"][cur_rule];
+
       httplib::url default_url(cur_rule_value["forward-url"].read<std::string>().c_str(), true);
       host_rule rule(cur_rule_value["match-path"].read<std::string>().c_str(), default_url);
       rules.add_rule(rule);
     }
-    **/
 
     host_rules_map[config["virtual-host"][i]["host-name"].read<std::string>()] = rules;
       
